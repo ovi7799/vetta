c44dc6b34a60a2209143d85b61adf8a3ed8c08f1
Ovi <ovihossain7799@gmail.com>
2024-09-28 13:40:02 +0600

    full project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
